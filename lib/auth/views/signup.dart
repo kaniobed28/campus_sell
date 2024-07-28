1514f1fd98d3e6bd1fd97c3a8439be3b6d4fa34e
Andrew Marfo <andrewmarfo23@gmail.com>
2024-07-28 15:12:18 +0000

    Centering items in signup and signin pages

--- lib/auth/views/signup.dart
+++ lib/auth/views/signup.dart
@@ -26,118 +26,121 @@ class Signup extends StatelessWidget {
       child: authController.isAuthenticated.isTrue
           ? const NewDashboard()
           : Scaffold(
-              body: Form(
-                key: _formKey,
-                child: Container(
-                  width: screenWidth,
-                  height: screenHeight,
-                  padding: const EdgeInsets.all(10),
-                  color: Colors.blueGrey[50],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(
-                          Icons.shopify_rounded,
-                          size: 80,
-                          color: Colors.black,
-                        ),
-                        onPressed: () {
-                          // Add functionality to the IconButton if needed
-                        },
-                      ),
-                      const Text(
-                        "Campus Sell",
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      // I have left empty height here
-                      SizedBox(
-                        height: 40,
-                        child: Text(
-                          "Create your online Shop Now",
-                          style: Theme.of(context).textTheme.displayMedium,
-                        ),
-                      ),
-
-                      const SizedBox(height: 92),
-                      emailFormWidget(emailOfFormController),
-                      const SizedBox(height: 60),
-                      passWrdFormWidget(passwordOfFormController),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.transparent),
+              body: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Container(
+                    width: screenWidth,
+                    height: screenHeight,
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.blueGrey[50],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(
+                            Icons.shopify_rounded,
+                            size: 80,
+                            color: Colors.black,
                           ),
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              // Add functionality for form submission here
-                              // For example, you can access the form data using:
-                              // emailOfFormController.text and passwordOfFormController.text
-                              try {
-                                await authController.signUpWithEmailAndPassword(
-                                    emailOfFormController.text.trim(),
-                                    passwordOfFormController.text.trim());
+                          onPressed: () {
+                            // Add functionality to the IconButton if needed
+                          },
+                        ),
+                        const Text(
+                          "Campus Sell",
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        // I have left empty height here
+                        SizedBox(
+                          height: 40,
+                          child: Text(
+                            "Create your online Shop Now",
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                        ),
 
-                                if (authController.uid.value.isNotEmpty) {
-                                  // print(authController.uid.isNotEmpty);
-                                  AdditionalInfoController
-                                      additionalInfoController =
-                                      Get.find<AdditionalInfoController>();
-                                  await additionalInfoController
-                                      .addDataToFirestore(
-                                          {}, authController.uid.value);
-                                  // authController.dispose();
-                                  Get.to(() => const NewDashboard());
-                                } else {
+                        const SizedBox(height: 92),
+                        emailFormWidget(emailOfFormController),
+                        const SizedBox(height: 60),
+                        passWrdFormWidget(passwordOfFormController),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.transparent),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                // Add functionality for form submission here
+                                // For example, you can access the form data using:
+                                // emailOfFormController.text and passwordOfFormController.text
+                                try {
+                                  await authController
+                                      .signUpWithEmailAndPassword(
+                                          emailOfFormController.text.trim(),
+                                          passwordOfFormController.text.trim());
+
+                                  if (authController.uid.value.isNotEmpty) {
+                                    // print(authController.uid.isNotEmpty);
+                                    AdditionalInfoController
+                                        additionalInfoController =
+                                        Get.find<AdditionalInfoController>();
+                                    await additionalInfoController
+                                        .addDataToFirestore(
+                                            {}, authController.uid.value);
+                                    // authController.dispose();
+                                    Get.to(() => const NewDashboard());
+                                  } else {
+                                    Get.snackbar(
+                                      'Somethng went wrong',
+                                      'Check your Internet connection. Password must be minimum of length 6,',
+                                      snackPosition: SnackPosition.BOTTOM,
+                                      duration: const Duration(seconds: 3),
+                                    ); //I have to work here
+                                  }
+                                } catch (e) {
                                   Get.snackbar(
                                     'Somethng went wrong',
-                                    'Check your Internet connection. Password must be minimum of length 6,',
+                                    'Check your Internet connection. Password must be minimum of length 6',
                                     snackPosition: SnackPosition.BOTTOM,
                                     duration: const Duration(seconds: 3),
-                                  ); //I have to work here
+                                  );
                                 }
-                              } catch (e) {
-                                Get.snackbar(
-                                  'Somethng went wrong',
-                                  'Check your Internet connection. Password must be minimum of length 6',
-                                  snackPosition: SnackPosition.BOTTOM,
-                                  duration: const Duration(seconds: 3),
-                                );
+                                // Navigator.push(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //       builder: (context) => SellInfoScreen()),
+                                // );
                               }
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //       builder: (context) => SellInfoScreen()),
-                              // );
-                            }
-                          },
-                          child: const Text(
-                            'Sign Up',
-                            style: TextStyle(
-                              color: Colors.black,
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Row(children: [
-                        const Text(
-                          "Already have a Shop? ",
-                        ),
-                        GestureDetector(
-                          onTap: () => Get.to(() => SignIn()),
-                          child: const Text(
-                            "Sign in",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w800),
+                        Row(children: [
+                          const Text(
+                            "Already have a Shop? ",
                           ),
-                        )
-                      ]),
-                    ],
+                          GestureDetector(
+                            onTap: () => Get.to(() => SignIn()),
+                            child: const Text(
+                              "Sign in",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          )
+                        ]),
+                      ],
+                    ),
                   ),
                 ),
               ),
